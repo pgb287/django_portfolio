b2270008595a1431929355eb0f03d6c69b46c979
Pablo Guillermo Bustos <pgb287@gmail.com>
2021-12-26 22:12:55 -0300

    proyectos, blog y post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100) 
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='blog/images')
+    fecha = models.DateField(datetime.date.today)
+
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def home(request):
+    # Traemos todos los registros de Project
+    projects = Project.objects.all()    
+    return render(request, 'home.html', {'projects':projects})
